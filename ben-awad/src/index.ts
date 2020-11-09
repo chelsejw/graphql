1e3c4396da578d0c3333b58ad6801f434ea345ac
chelsejw <chelsejw@gmail.com>
2020-11-10 00:41:23 +0800

    add benawad tutorial, basic setup with mikro-orm and typescript

---
+++ ben-awad/src/index.ts
@@ -0,0 +1,17 @@
+import { MikroORM } from "@mikro-orm/core";
+import { Post } from "./entities/Post";
+import microConfig from './mikro-orm.config'
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+
+  const post = orm.em.create(Post, {title: 'My first post!'});
+  await orm.em.persistAndFlush(post);
+  await orm.em.nativeInsert(Post, {title: 'My second first post :)'});
+
+};
+
+main()
+.catch(err => {
+  console.log(err)
+});
